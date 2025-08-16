e238fb3e25454b08bfb84ccfb31599464b2f23c7
ozncncyr <ozncncyr@outlook.com>
2025-08-16 20:50:56 +0300

    fix: correct username property in Profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import userData from "./data/userData.json";
-import friends from "./data/friends.json";
-import React from "react";
-import transactions from "./data/transactions.json";
-import TransactionHistory from "./components/transactionHistory/TransactionHistory";
+import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
+import userData from './data/userData.json';
+import friends from './data/friends.json';
+import React from 'react';
+import transactions from './data/transactions.json';
+import TransactionHistory from './components/transactionHistory/TransactionHistory';
 
 function App() {
   return (
     <>
       <Profile
-        name={userData.name}
+        name={userData.username}
         tag={userData.tag}
         location={userData.location}
         image={userData.avatar}
